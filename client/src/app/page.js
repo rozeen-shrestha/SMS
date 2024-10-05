77cda31a3cb4fb35f6833e9ed81abe2516395d6b
rozeen-shrestha <rozeeen.shrestha@gmail.com>
2024-10-05 18:00:17 +0545

    minor config console error fix and homepage button

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const page = () => {
+const Page = () => {
   return (
-    <div>page</div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-xl mb-4">SMS v0.1</div>
+      <button className="bg-blue-500 text-white py-2 px-4 rounded">Dashboard</button>
+    </div>
   )
 }
 
-export default page
+export default Page
