e5ff32bcba757ce625b4f9c33570218739a7cb62
rozeen-shrestha <rozeeen.shrestha@gmail.com>
2024-10-05 17:05:02 +0545

    Revert "config error"
    This reverts commit d6ce98bf6db880573b4586eac72dce3cfcedc929.

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Enables React's Strict Mode
     api: {
         externalResolver: true,
-    },
-    // Add other configurations as needed
+      },
 };
 
 export default nextConfig;
