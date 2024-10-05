a0c2dc1ad05547d8689368a6d5b7bbbba4a388f1
rozeen-shrestha <rozeeen.shrestha@gmail.com>
2024-10-05 18:11:09 +0545

    dashboard

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,25 @@
+"use client"
 import React from 'react'
+import { useRouter } from 'next/router'
 
-const page = () => {
+const Page = () => {
+  const router = useRouter();
+
+  const handleDashboardClick = () => {
+    router.push('/admin');
+  }
+
   return (
-    <div>page</div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-xl mb-4">SMS v0.1</div>
+      <button
+        className="bg-blue-500 text-white py-2 px-4 rounded"
+        onClick={handleDashboardClick}
+      >
+        Dashboard
+      </button>
+    </div>
   )
 }
 
-export default page
+export default Page
