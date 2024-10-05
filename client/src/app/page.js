e23e1e35903043e089fde86c36273f567546b122
rozeen-shrestha <rozeeen.shrestha@gmail.com>
2024-10-05 18:06:25 +0545

    minor config console error fix and homepage button

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,26 @@
+'use client'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
-const page = () => {
+
+const Page = () => {
+  const router = useRouter();
+
+  const handleDashboardClick = () => {
+    router.push('/admin');
+  }
+
   return (
-    <div>page</div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-xl mb-4">SMS v0.1</div>
+      <button
+        className="bg-blue-500 text-white py-2 px-4 rounded"
+        onClick={handleDashboardClick}
+      >
+        Dashboard
+      </button>
+    </div>
   )
 }
 
-export default page
+export default Page
