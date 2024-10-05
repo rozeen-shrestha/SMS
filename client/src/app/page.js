0aa3fc964ea98443ed4a52278310378f06aa4eec
rozeen-shrestha <rozeeen.shrestha@gmail.com>
2024-10-05 18:07:22 +0545

    Revert "minor config console error fix and homepage button"
    This reverts commit e23e1e35903043e089fde86c36273f567546b122.

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,26 +1,9 @@
-'use client'
-import { useRouter } from 'next/navigation';
 import React from 'react'
 
-
-const Page = () => {
-  const router = useRouter();
-
-  const handleDashboardClick = () => {
-    router.push('/admin');
-  }
-
+const page = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="text-xl mb-4">SMS v0.1</div>
-      <button
-        className="bg-blue-500 text-white py-2 px-4 rounded"
-        onClick={handleDashboardClick}
-      >
-        Dashboard
-      </button>
-    </div>
+    <div>page</div>
   )
 }
 
-export default Page
+export default page
